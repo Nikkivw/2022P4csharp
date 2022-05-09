c7998f8da228495815ba9206058a489e42e42a2c
Nikkivw <32580@ma-web.nl>
2022-05-09 12:07:55 +0200

    list bronz en silver afgemaakt

--- Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
+++ Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    List<string> birds = new List<string>() {"Duif", "Roodborsje" };
+    birds.Add("Uil");
+    birds.Add("Kraai");
+    birds.Add("Papagaai");
+
+    birds.Remove("Hond");
+    
+    List<string> birds2 = new List<string>();
+    birds2.Add("meeuw");
+    birds2.Add("Arend");
+ 
+    foreach (string bird in birds)
+    {
+        Console.WriteLine(bird);
+    }
+
+    Console.ReadLine();
